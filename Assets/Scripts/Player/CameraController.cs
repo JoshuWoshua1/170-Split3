d2d203df96cc09a1e61d460ee41709db470258df
Joshua Kim-Pearson <jkimpear@ucsc.edu>
2026-05-05 20:41:22 -0700

    lerp lerp lerp
    camera is smoothd

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
+using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform cameraPivot;
     [SerializeField] private float baseOrthographicSize = 5f;
-    [SerializeField] private float cameraScaleMultiplier = 0.5f;
+    [SerializeField] private float cameraScaleMultiplier = 0.8f;
     [SerializeField] private Camera mainCamera;
 
     private Vector3 positionOffset;
@@ -32,7 +33,21 @@ public class CameraController : MonoBehaviour
     
     public void ScaleCamera(float playerSize)
     {
-        float scaleFactor = 1f + (playerSize - 1f) * cameraScaleMultiplier;
-        mainCamera.orthographicSize = baseOrthographicSize * scaleFactor;
+        float targetSize = baseOrthographicSize + (playerSize - 1f) * cameraScaleMultiplier;
+        mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, targetSize, Time.deltaTime);
+    }
+    private IEnumerator SmoothScale(Vector3 targetScale, float duration)
+    {
+        Vector3 initialScale = transform.localScale;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            transform.localScale = Vector3.Lerp(initialScale, targetScale, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localScale = targetScale;
     }
 }
