fe156783f1f2e32aa4ed7445a5644c8ab6db4fb0
mydoot <buaqur@gmail.com>
2026-05-08 11:41:09 -0700

    Individual elements in UI only shows when first ddiscovered

--- Assets/Scripts/ElementUI.cs
+++ Assets/Scripts/ElementUI.cs
@@ -6,16 +6,57 @@ public class ElementUI : MonoBehaviour
 
     [SerializeField] private TextMeshProUGUI Name;
     [SerializeField] private TextMeshProUGUI Amount;
-    //[SerializeField] private Image Icon;
+    //[SerializeField] private Image Icon; //add a way to load an icon
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private int amnt = 0;
+    //private ResourceType resourceName;
+
+    private void Update()
     {
-        
+        //this.amnt = Resource.Instance.GetResourceAmount(resourceName);
+
+        this.Amount.text = amnt.ToString();
+
+        /*if (this.amnt > 0)
+        {
+            this.SetActive(true);
+        } else
+        {
+            this.SetActive(false);
+        }*/
+    }
+
+    public void setAmount(int num, string type)
+    {
+        if (type == null)
+        {
+            Debug.LogWarning("Did not set type for setAmount().");
+            return;
+        }
+
+        switch (type)
+        {
+            case "add":
+                this.amnt += num;
+                break;
+
+            case "set":
+                this.amnt = num;
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public int getAmount()
+    {
+        return amnt;
     }
 
     public void Load(ResourceType name, int amount)
     {
+        //this.resourceName = name;
         this.Name.text = name.ToString();
         this.Amount.text = amount.ToString();
     }
--- Assets/Scripts/ResourceUIScript.cs
+++ Assets/Scripts/ResourceUIScript.cs
@@ -8,6 +8,7 @@ public class ResourceUIScript : MonoBehaviour
     [SerializeField] private ElementUI UIPrefab;
     [SerializeField] private Transform UITransform;
 
+    private Dictionary<ResourceType, ElementUI> UIElements = new Dictionary<ResourceType, ElementUI>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,7 +24,10 @@ public class ResourceUIScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        foreach (ResourceType type in System.Enum.GetValues(typeof(ResourceType)))
+        {
+            checkResource(type, ResourceManager.Instance.GetResourceAmount(type));
+        }
     }
 
     void InitializeUI()
@@ -32,6 +36,21 @@ public class ResourceUIScript : MonoBehaviour
         {
             ElementUI newElement = Instantiate(UIPrefab, UITransform);
             newElement.Load(type, ResourceManager.Instance.GetResourceAmount(type));
+            newElement.gameObject.SetActive(false);
+
+            UIElements.Add(type, newElement);
+        }
+    }
+
+    void checkResource(ResourceType type, int amnt)
+    {
+        if (UIElements.TryGetValue(type, out ElementUI UI))
+        {
+            if (UI.getAmount() > 0 && !UI.gameObject.activeSelf)
+            {
+                UI.gameObject.SetActive(true);
+            }
+            UI.setAmount(amnt, "set");
         }
     }
 }
